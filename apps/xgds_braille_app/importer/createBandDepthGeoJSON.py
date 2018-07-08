48fff682bcc4826fffe7189d954b5322c770a80b
Khaled Sharif <khaled.sharif@arabiaweather.com>
2018-07-07 20:28:46 -0700

    Added script to convert band depth time series to a GeoJSON

---
+++ apps/xgds_braille_app/importer/createBandDepthGeoJSON.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+'''
+This script requires some additional dependencies, which are installed using:
+pip install pandas utm colour
+'''
+
+import django
+
+from geocamUtil.loader import LazyGetModelByName
+
+django.setup()
+from django.conf import settings
+
+from xgds_braille_app.models import BandDepthDefinition, BandDepthGeoJSON, BandDepthTimeSeries
+
+import pandas as pd
+from colour import Color
+from utm import from_latlon, to_latlon
+from json import dumps
+
+colors = list(Color("blue").range_to(Color("red"), 100))
+
+def lat_lon_to_utm(row):
+    return from_latlon(row['latitude'], row['longitude'])
+
+def clip_to_range(minimum, maximum, x):
+    return max(minimum, min(x, maximum))
+
+def scale_between(minimum, maximum, x):
+    rng = maximum - minimum
+    return minimum + x * rng
+
+def get_color(percentage):
+    clipped = clip_to_range(0, 0.5, percentage)
+    clipped *= 2 # it is now between 0 and 1
+    return colors[int(clipped * (len(colors) - 1))]
+
+def create_feature_collection(collection):
+    return {
+        "type": "FeatureCollection",
+        "features": collection,
+    }
+
+def create_geojson(easting, northing, zone_number, zone_letter, band_depth, confidence, stddev):
+    clipped_confidence = clip_to_range(0, 150, confidence) / 150.0
+    radius = scale_between(0.25, 0.5, clipped_confidence)
+
+    lat_minus_radius, lng_minus_radius = to_latlon(easting - radius, northing - radius, zone_number, zone_letter)
+    lat_plus_radius,  lng_plus_radius  = to_latlon(easting + radius, northing + radius, zone_number, zone_letter)
+
+    return {
+        "type": "Feature",
+        "properties": 
+        {
+            "stroke-width": 0,
+            "fill": str(get_color(band_depth)),
+            "fill-opacity": 1,
+            "band-depth": "%f +- %f" % (round(float(band_depth), 4), round(float(stddev), 4)),
+            "confidence": int(confidence),
+        },
+        "geometry":
+        {
+            "type": "Polygon",
+            "coordinates": 
+            [[
+                [lng_plus_radius,  lat_minus_radius],
+                [lng_minus_radius, lat_minus_radius],
+                [lng_minus_radius, lat_plus_radius],
+                [lng_plus_radius,  lat_plus_radius],
+                [lng_plus_radius,  lat_minus_radius],
+            ]]
+        }
+    }
+
+def create_geojson_for_flight(flight, band_depth_definition):
+    band_depth_time_series = BandDepthTimeSeries.objects.filter(
+        time_stamp__gte=flight.start_time,
+        time_stamp__lte=flight.end_time,
+        band_depth_definition=band_depth_definition,
+        flight=flight,
+    )
+    band_depth = []
+    for bdts in band_depth_time_series:
+        band_depth.append({
+            "timestamp": bdts.time_stamp,
+            "value": bdts.band_depth,
+        })
+    band_depth = pd.DataFrame(data=band_depth)
+
+    flight_track_positions = flight.track.getPositions()
+    gps = []
+    for ftp in flight_track_positions:
+        gps.append({
+            "timestamp": ftp.timestamp,
+            "latitude": ftp.latitude,
+            "longitude": ftp.longitude,
+        })
+    gps = pd.DataFrame(data=gps)
+
+    band_depth.index = pd.to_datetime(
+        band_depth['timestamp'],
+        infer_datetime_format=True,
+        utc=True,
+    )
+    band_depth.sort_index(inplace=True)
+
+    gps.index = pd.to_datetime(
+        gps['timestamp'],
+        infer_datetime_format=True,
+        utc=True,
+    )
+    gps.sort_index(inplace=True)
+
+    merged_df = pd.merge_asof(
+        band_depth,
+        gps,
+        left_index=True,
+        right_index=True,
+        direction="nearest",
+        suffixes=["_band_depth", "_gps"],
+    )[[
+        "latitude",
+        "longitude",
+        "value",
+    ]]
+
+    post_df = merged_df.apply(lat_lon_to_utm, axis=1)
+
+    zone_number, zone_letter = post_df.iloc[0][2], post_df.iloc[0][3]
+
+    eastings = post_df.apply(lambda x: x[0]).values
+    northings = post_df.apply(lambda x: x[1]).values
+
+    merged_df['easting'] = [int(round(x)) for x in eastings]
+    merged_df['northing'] = [int(round(x)) for x in northings]
+
+    merged_df = merged_df[['easting', 'northing', 'value']]
+
+    groups = merged_df.groupby(['easting', 'northing'])
+
+    grouped_df = (
+         groups
+        .mean().rename(index=str, columns={"value": "mean"})
+        .join(groups.count().rename(index=str, columns={"value": "count"}), how="left")
+        .join(groups.std().rename(index=str, columns={"value": "std"}), how="left")
+    )
+
+    grouped_df.reset_index(inplace=True)
+
+    geojson_collection = []
+
+    for element_index in range(len(grouped_df)):
+        element = grouped_df.iloc[element_index]
+
+        geojson_collection.append(create_geojson(
+            easting=int(element.at['easting']),
+            northing=int(element.at['northing']),
+            zone_number=zone_number,
+            zone_letter=zone_letter,
+            band_depth=element.at['mean'],
+            confidence=element.at['count'],
+            stddev=element.at['std'],
+        ))
+
+    return dumps(create_feature_collection(geojson_collection))
+
+if __name__ == "__main__":
+    all_flights = LazyGetModelByName(settings.XGDS_CORE_FLIGHT_MODEL).get().objects.all()
+    all_band_depth_definitions = BandDepthDefinition.objects.all()
+
+    for flight in all_flights:
+        for bdd in all_band_depth_definitions:
+            geojson_string = create_geojson_for_flight(flight, bdd)
+            BandDepthGeoJSON.objects.create(
+                flight=flight,
+                band_depth_definition=bdd,
+            )
